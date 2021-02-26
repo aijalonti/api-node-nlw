9295882484e88b49b6394185fe02ee05b9d2e117
aijalonti <aijalon.ti@gmail.com>
2021-02-26 10:45:46 -0300

    feat: create repository user and services surveys

--- src/server.ts
+++ src/server.ts
@@ -8,4 +8,4 @@ const app = express();
 app.use(express.json());
 app.use(router);
 
-app.listen(3333, () => console.log("Server is running!"));
+app.listen(3535, () => console.log("Server is running!"));
